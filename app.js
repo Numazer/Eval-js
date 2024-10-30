319672256ae5c751993918d1877f3b93ef5034d0
numa <emmanuel.saadisarrault@gmail.com>
2024-10-30 12:29:22 +0100

    html Finit debut javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+let listeTache = document.getElementById('listeTache');
+
+function createTache() {
+    let creerTache = document.getElementById('creerTache');
+    let textTache = creerTache.value;
+}
+
+let li = document.createElement("li");
+li.innerHTML = textTache;
+
